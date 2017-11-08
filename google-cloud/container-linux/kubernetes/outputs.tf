4ec6732b982a918d8216368b821f303e64a8ec6f
Dalton Hubble <dghubble@gmail.com>
2017-11-08 00:19:49 -0800

    Output the Google network name and self_link
    * Allow users to add custom firewall rules for unique cases

--- google-cloud/container-linux/kubernetes/outputs.tf
+++ google-cloud/container-linux/kubernetes/outputs.tf
@@ -5,3 +5,11 @@ output "controllers_ipv4_public" {
 output "ingress_static_ip" {
   value = "${module.workers.ingress_static_ip}"
 }
+
+output "network_name" {
+  value = "${google_compute_network.network.name}"
+}
+
+output "network_self_link" {
+  value = "${google_compute_network.network.self_link}"
+}
