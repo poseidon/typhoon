26c65d38067180c7e73e78664fd68d16376b8cc9
Barak Michener <me@barakmich.com>
2017-12-09 15:09:31 -0800

    aws: Support custom cluster domains

--- aws/container-linux/kubernetes/controllers.tf
+++ aws/container-linux/kubernetes/controllers.tf
@@ -54,6 +54,7 @@ data "template_file" "controller_config" {
 
     k8s_dns_service_ip      = "${cidrhost(var.service_cidr, 10)}"
     ssh_authorized_key      = "${var.ssh_authorized_key}"
+    cluster_dns_fqdn        = "${var.cluster_dns_fqdn}"
     kubeconfig_ca_cert      = "${module.bootkube.ca_cert}"
     kubeconfig_kubelet_cert = "${module.bootkube.kubelet_cert}"
     kubeconfig_kubelet_key  = "${module.bootkube.kubelet_key}"
