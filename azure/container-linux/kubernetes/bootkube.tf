fcf738ea780d21c1863bb08061dfad654c88f1a6
Stephen Augustus <foo@agst.us>
2018-01-07 12:09:41 -0500

    azure: Fix etcd list reference in bootkube

--- azure/container-linux/kubernetes/bootkube.tf
+++ azure/container-linux/kubernetes/bootkube.tf
@@ -1,10 +1,10 @@
 # Self-hosted Kubernetes assets (kubeconfig, manifests)
 module "bootkube" {
-  source = "git::https://github.com/poseidon/terraform-render-bootkube.git?ref=v0.9.1"
+  source = "git::https://github.com/poseidon/terraform-render-bootkube.git?ref=5072569bb7dff1c2f6bc6fb7b06ce0a41809971e"
 
   cluster_name          = "${var.cluster_name}"
   api_servers           = ["${format("%s.%s", var.cluster_name, var.dns_zone)}"]
-  etcd_servers          = ["${null_resource.repeat.*.triggers.domain}"]
+  etcd_servers          = "${null_resource.repeat.*.triggers.domain}"
   asset_dir             = "${var.asset_dir}"
   networking            = "${var.networking}"
   network_mtu           = "${var.network_mtu}"
