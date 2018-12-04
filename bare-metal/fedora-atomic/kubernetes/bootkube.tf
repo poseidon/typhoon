76d993cdaefc2d6b88bc524bdec69ffc152d974b
Dalton Hubble <dghubble@gmail.com>
2018-12-03 19:50:06 -0800

    Add experimental kube-router CNI provider
    * Add kube-router for pod networking and NetworkPolicy
    as an experiment
    * Experiments are not documented or supported in any way,
    and may be removed without notice. They have known issues
    and aren't enabled without special options.

--- bare-metal/fedora-atomic/kubernetes/bootkube.tf
+++ bare-metal/fedora-atomic/kubernetes/bootkube.tf
@@ -1,6 +1,6 @@
 # Self-hosted Kubernetes assets (kubeconfig, manifests)
 module "bootkube" {
-  source = "git::https://github.com/poseidon/terraform-render-bootkube.git?ref=4021467b7f280ceb54320333690e8574a3bd8d84"
+  source = "git::https://github.com/poseidon/terraform-render-bootkube.git?ref=9d6f0c31d36f8e84e9f7187f5fddf5e344b31f56"
 
   cluster_name          = "${var.cluster_name}"
   api_servers           = ["${var.k8s_domain_name}"]
