1ab06f69d7db0324a8069090cc83ea3b1d45e790
Dalton Hubble <dghubble@gmail.com>
2019-01-29 21:51:25 -0800

    Update flannel from v0.10.0 to v0.11.0
    * https://github.com/coreos/flannel/releases/tag/v0.11.0

--- bare-metal/fedora-atomic/kubernetes/bootkube.tf
+++ bare-metal/fedora-atomic/kubernetes/bootkube.tf
@@ -1,6 +1,6 @@
 # Self-hosted Kubernetes assets (kubeconfig, manifests)
 module "bootkube" {
-  source = "git::https://github.com/poseidon/terraform-render-bootkube.git?ref=54f15b6c8c4fa14a3f8ae3f58036a7b51e47ef87"
+  source = "git::https://github.com/poseidon/terraform-render-bootkube.git?ref=c5bc23ef7a3f5d8e5be976ee60b58dbbfcc3e7c9"
 
   cluster_name          = "${var.cluster_name}"
   api_servers           = ["${var.k8s_domain_name}"]
