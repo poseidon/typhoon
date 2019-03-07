c9ad1d03423a2dba35422ad7c14c855ca577b228
7ko7 <43700584+7ko7@users.noreply.github.com>
2019-03-07 12:01:53 +0000

    Update bootkube.tf
    Referencing 7ko7 bootkube render to pick up cert length changes

--- bare-metal/container-linux/kubernetes/bootkube.tf
+++ bare-metal/container-linux/kubernetes/bootkube.tf
@@ -1,6 +1,6 @@
 # Self-hosted Kubernetes assets (kubeconfig, manifests)
 module "bootkube" {
-  source = "git::https://github.com/poseidon/terraform-render-bootkube.git?ref=1528266595b91d40f824a3281563f13dd0a5b6df"
+  source = "git::https://github.com/7ko7/terraform-render-bootkube.git?ref=1528266595b91d40f824a3281563f13dd0a5b6df"
 
   cluster_name                    = "${var.cluster_name}"
   api_servers                     = ["${var.k8s_domain_name}"]
