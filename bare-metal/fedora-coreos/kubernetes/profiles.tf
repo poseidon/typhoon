5fdeb9bc78394b42d1812f9104fc295f376a86cb
Dalton Hubble <dghubble@gmail.com>
2019-07-18 00:58:11 -0700

    Adjust Fedora CoreOS image locations
    * Use the xz compressed images published by Fedora testing,
    instead of gzippped tarballs. This is possible because the
    initramfs now supports xz and coreos-installer 0.8 was added
    * Separate bios and uefi raw images are no longer needed

--- bare-metal/fedora-coreos/kubernetes/profiles.tf
+++ bare-metal/fedora-coreos/kubernetes/profiles.tf
@@ -1,11 +1,11 @@
 locals {
-  remote_kernel = "https://builds.coreos.fedoraproject.org/prod/streams/${var.os_stream}/builds/${var.os_version}/fedora-coreos-${var.os_version}-installer-kernel"
-  remote_initrd = "https://builds.coreos.fedoraproject.org/prod/streams/${var.os_stream}/builds/${var.os_version}/fedora-coreos-${var.os_version}-installer-initramfs.img"
+  remote_kernel = "https://builds.coreos.fedoraproject.org/prod/streams/${var.os_stream}/builds/${var.os_version}/x86_64/fedora-coreos-${var.os_version}-installer-kernel"
+  remote_initrd = "https://builds.coreos.fedoraproject.org/prod/streams/${var.os_stream}/builds/${var.os_version}/x86_64/fedora-coreos-${var.os_version}-installer-initramfs.img"
   remote_args = [
     "ip=dhcp",
     "rd.neednet=1",
     "coreos.inst=yes",
-    "coreos.inst.image_url=https://builds.coreos.fedoraproject.org/prod/streams/${var.os_stream}/builds/${var.os_version}/fedora-coreos-${var.os_version}-metal.raw.gz",
+    "coreos.inst.image_url=https://builds.coreos.fedoraproject.org/prod/streams/${var.os_stream}/builds/${var.os_version}/x86_64/fedora-coreos-${var.os_version}-metal.raw.xz",
     "coreos.inst.ignition_url=${var.matchbox_http_endpoint}/ignition?uuid=$${uuid}&mac=$${mac:hexhyp}",
     "coreos.inst.install_dev=${var.install_disk}"
   ]
@@ -16,7 +16,7 @@ locals {
     "ip=dhcp",
     "rd.neednet=1",
     "coreos.inst=yes",
-    "coreos.inst.image_url=${var.matchbox_http_endpoint}/assets/fedora-coreos/fedora-coreos-${var.os_version}-metal-bios.raw.gz",
+    "coreos.inst.image_url=${var.matchbox_http_endpoint}/assets/fedora-coreos/fedora-coreos-${var.os_version}-metal.raw.xz",
     "coreos.inst.ignition_url=${var.matchbox_http_endpoint}/ignition?uuid=$${uuid}&mac=$${mac:hexhyp}",
     "coreos.inst.install_dev=${var.install_disk}"
   ]
@@ -44,7 +44,8 @@ resource "matchbox_profile" "controllers" {
 data "ct_config" "controller-ignitions" {
   count = length(var.controller_names)
 
-  content      = data.template_file.controller-configs.*.rendered[count.index]
+  content = data.template_file.controller-configs.*.rendered[count.index]
+  strict  = true
 }
 
 data "template_file" "controller-configs" {
@@ -78,7 +79,8 @@ resource "matchbox_profile" "workers" {
 data "ct_config" "worker-ignitions" {
   count = length(var.worker_names)
 
-  content      = data.template_file.worker-configs.*.rendered[count.index]
+  content = data.template_file.worker-configs.*.rendered[count.index]
+  strict  = true
 }
 
 data "template_file" "worker-configs" {
