fd12f3612bc63e5782b1f36c8f01e61203f30b5c
Dalton Hubble <dghubble@gmail.com>
2019-09-14 16:54:08 -0700

    Rename CA organization from bootkube to typhoon
    * Rename the organization in generated CA certificates from
    bootkube to typhoon. Avoid confusion with the bootkube project
    * https://github.com/poseidon/terraform-render-bootstrap/pull/149

--- bare-metal/fedora-coreos/kubernetes/bootkube.tf
+++ bare-metal/fedora-coreos/kubernetes/bootkube.tf
@@ -1,6 +1,6 @@
 # Kubernetes assets (kubeconfig, manifests)
 module "bootstrap" {
-  source = "git::https://github.com/poseidon/terraform-render-bootstrap.git?ref=6e59af71138bc5f784453873074de16e7ee150eb"
+  source = "git::https://github.com/poseidon/terraform-render-bootstrap.git?ref=d6206abedd69aca2e362bf533f73b13805ea344c"
 
   cluster_name                    = var.cluster_name
   api_servers                     = [var.k8s_domain_name]
