0ba2c1a4dace6efaf9e1bc035ae9cff0ca20ca55
Dalton Hubble <dghubble@gmail.com>
2020-06-29 23:04:54 -0700

    Fix terraform fmt in firewall rules

--- azure/container-linux/kubernetes/security.tf
+++ azure/container-linux/kubernetes/security.tf
@@ -117,7 +117,7 @@ resource "azurerm_network_security_rule" "controller-apiserver" {
 
 resource "azurerm_network_security_rule" "controller-cilium-health" {
   resource_group_name = azurerm_resource_group.cluster.name
-  count = var.networking == "cilium" ? 1 : 0
+  count               = var.networking == "cilium" ? 1 : 0
 
   name                        = "allow-cilium-health"
   network_security_group_name = azurerm_network_security_group.controller.name
@@ -299,7 +299,7 @@ resource "azurerm_network_security_rule" "worker-https" {
 
 resource "azurerm_network_security_rule" "worker-cilium-health" {
   resource_group_name = azurerm_resource_group.cluster.name
-  count = var.networking == "cilium" ? 1 : 0
+  count               = var.networking == "cilium" ? 1 : 0
 
   name                        = "allow-cilium-health"
   network_security_group_name = azurerm_network_security_group.worker.name
