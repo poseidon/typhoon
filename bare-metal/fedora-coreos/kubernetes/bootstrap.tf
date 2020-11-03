82e5ac3e7c82c594f7b75dc66368402fb3881068
Dalton Hubble <dghubble@gmail.com>
2020-11-03 10:29:07 -0800

    Update Cilium from v1.8.5 to v1.9.0-rc3
    * https://github.com/poseidon/terraform-render-bootstrap/pull/224

--- bare-metal/fedora-coreos/kubernetes/bootstrap.tf
+++ bare-metal/fedora-coreos/kubernetes/bootstrap.tf
@@ -1,6 +1,6 @@
 # Kubernetes assets (kubeconfig, manifests)
 module "bootstrap" {
-  source = "git::https://github.com/poseidon/terraform-render-bootstrap.git?ref=3a0feda171eee0db272f265241a3e7d15271bdce"
+  source = "git::https://github.com/poseidon/terraform-render-bootstrap.git?ref=c6e3a2bcdc06aff31b1f83c91b2f0af4f778b86f"
 
   cluster_name                    = var.cluster_name
   api_servers                     = [var.k8s_domain_name]
