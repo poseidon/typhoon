861021ee988667b93920ca149356aacc818c7692
Dalton Hubble <dghubble@gmail.com>
2021-12-09 09:28:41 -0800

    Update Kubernetes from v1.22.4 to v1.23.0
    * https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG/CHANGELOG-1.23.md#v1230
    * With Calico, add missing caliconodestatuses CRD added in v3.21.0
    https://github.com/poseidon/terraform-render-bootstrap/pull/289

--- bare-metal/flatcar-linux/kubernetes/bootstrap.tf
+++ bare-metal/flatcar-linux/kubernetes/bootstrap.tf
@@ -1,6 +1,6 @@
 # Kubernetes assets (kubeconfig, manifests)
 module "bootstrap" {
-  source = "git::https://github.com/poseidon/terraform-render-bootstrap.git?ref=cb1f4410ed8e5793622406e4faa5d1abf2dd0829"
+  source = "git::https://github.com/poseidon/terraform-render-bootstrap.git?ref=362158a6d60aa16ef81eab347b1bb5268db652e2"
 
   cluster_name                    = var.cluster_name
   api_servers                     = [var.k8s_domain_name]
