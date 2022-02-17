fc38ba45b1c5547d4b31f543f38e41861d60f1f9
Dalton Hubble <dghubble@gmail.com>
2022-02-17 09:00:31 -0800

    Update Kubernetes from v1.23.3 to v1.23.4
    * https://github.com/kubernetes/kubernetes/blob/master/CHANGELOG/CHANGELOG-1.23.md#v1234

--- bare-metal/fedora-coreos/kubernetes/bootstrap.tf
+++ bare-metal/fedora-coreos/kubernetes/bootstrap.tf
@@ -1,6 +1,6 @@
 # Kubernetes assets (kubeconfig, manifests)
 module "bootstrap" {
-  source = "git::https://github.com/poseidon/terraform-render-bootstrap.git?ref=26bea83b957d18b99e16435405983181c4a6e159"
+  source = "git::https://github.com/poseidon/terraform-render-bootstrap.git?ref=205e5f212b634b3f590c0a91f60f4540b145045d"
 
   cluster_name                    = var.cluster_name
   api_servers                     = [var.k8s_domain_name]
