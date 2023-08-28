f6367876530a26c7a456bc6d390ea2d9a3cdf457
Dalton Hubble <dghubble@gmail.com>
2023-08-27 20:38:31 -0700

    Configure Cilium agents to connect to apiserver explicitly
    * Details in https://github.com/poseidon/terraform-render-bootstrap/pull/365

--- digital-ocean/fedora-coreos/kubernetes/bootstrap.tf
+++ digital-ocean/fedora-coreos/kubernetes/bootstrap.tf
@@ -1,6 +1,6 @@
 # Kubernetes assets (kubeconfig, manifests)
 module "bootstrap" {
-  source = "git::https://github.com/poseidon/terraform-render-bootstrap.git?ref=0065e511c5004e552e2e0e1111cc8039683a1eaa"
+  source = "git::https://github.com/poseidon/terraform-render-bootstrap.git?ref=6a5b610708776161515a2a42d793b6083575bf31"
 
   cluster_name = var.cluster_name
   api_servers  = [format("%s.%s", var.cluster_name, var.dns_zone)]
