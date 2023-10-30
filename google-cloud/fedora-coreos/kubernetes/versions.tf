435fa196da116ec9b033ea2244914ab681f3892d
Dalton Hubble <dghubble@gmail.com>
2023-10-29 21:05:49 -0700

    Relax the provider version constraint for Google Cloud
    * Allow upgrading to the v5.x Google Cloud Terrform Provider
    * Relax the version constraint to ease future compatibility,
    though it does allow users to upgrade prematurely

--- google-cloud/fedora-coreos/kubernetes/versions.tf
+++ google-cloud/fedora-coreos/kubernetes/versions.tf
@@ -3,7 +3,7 @@
 terraform {
   required_version = ">= 0.13.0, < 2.0.0"
   required_providers {
-    google = ">= 2.19, < 5.0"
+    google = ">= 2.19"
     null   = ">= 2.1"
     ct = {
       source  = "poseidon/ct"
