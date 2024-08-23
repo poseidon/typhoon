1c4ff9b7dad0e85042976000d119207d663434d9
Dalton Hubble <dghubble@gmail.com>
2024-08-23 13:06:47 -0700

    Remove upper bound on azurerm provider version
    * Allow folks to start upgrading to azurerm provider v4.0.0,
    don't set an upper bound on versions going forward

--- azure/fedora-coreos/kubernetes/versions.tf
+++ azure/fedora-coreos/kubernetes/versions.tf
@@ -3,7 +3,7 @@
 terraform {
   required_version = ">= 0.13.0, < 2.0.0"
   required_providers {
-    azurerm = ">= 2.8, < 4.0"
+    azurerm = ">= 2.8"
     null    = ">= 2.1"
     ct = {
       source  = "poseidon/ct"
