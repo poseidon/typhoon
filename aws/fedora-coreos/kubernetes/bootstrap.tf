552dbb40010448c0a8e9c2d488adb3bbf1c1c8f1
Dalton Hubble <dghubble@gmail.com>
2025-07-27 14:22:49 -0700

    Rollback Cilium from v1.17.6 to v1.17.5
    * Cilium v1.17.6 is broken, see https://github.com/cilium/cilium/issues/40571

--- aws/fedora-coreos/kubernetes/bootstrap.tf
+++ aws/fedora-coreos/kubernetes/bootstrap.tf
@@ -1,6 +1,6 @@
 # Kubernetes assets (kubeconfig, manifests)
 module "bootstrap" {
-  source = "git::https://github.com/poseidon/terraform-render-bootstrap.git?ref=cd82a41654ecfd40d6c44d426356012b9f8f806f"
+  source = "git::https://github.com/poseidon/terraform-render-bootstrap.git?ref=c7e2a637d7090d9e8f98a146a39872cfc546997c"
 
   cluster_name           = var.cluster_name
   api_servers            = [format("%s.%s", var.cluster_name, var.dns_zone)]
--- bare-metal/flatcar-linux/kubernetes/bootstrap.tf
+++ bare-metal/flatcar-linux/kubernetes/bootstrap.tf
@@ -1,6 +1,6 @@
 # Kubernetes assets (kubeconfig, manifests)
 module "bootstrap" {
-  source = "git::https://github.com/poseidon/terraform-render-bootstrap.git?ref=cd82a41654ecfd40d6c44d426356012b9f8f806f"
+  source = "git::https://github.com/poseidon/terraform-render-bootstrap.git?ref=c7e2a637d7090d9e8f98a146a39872cfc546997c"
 
   cluster_name           = var.cluster_name
   api_servers            = [var.k8s_domain_name]
