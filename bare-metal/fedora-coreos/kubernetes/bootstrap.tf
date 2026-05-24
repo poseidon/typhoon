841683ca4d5c0b9c9b2850448364739b2ca763fd
Dalton Hubble <dghubble@gmail.com>
2026-05-23 22:30:47 -0700

    Update terraform-render-bootstrap for bug fix
    * Avoid template error when cloud_provider is left as null,
    the default
    * https://github.com/poseidon/terraform-render-bootstrap/pull/426

--- bare-metal/fedora-coreos/kubernetes/bootstrap.tf
+++ bare-metal/fedora-coreos/kubernetes/bootstrap.tf
@@ -1,6 +1,6 @@
 # Kubernetes assets (kubeconfig, manifests)
 module "bootstrap" {
-  source = "git::https://github.com/poseidon/terraform-render-bootstrap.git?ref=5bf3975fdb13524267b8e81988874f9c540a7c5e"
+  source = "git::https://github.com/poseidon/terraform-render-bootstrap.git?ref=e958673eb6561b0a975f22f3578875835b8eda0e"
 
   cluster_name           = var.cluster_name
   api_servers            = [var.k8s_domain_name]
